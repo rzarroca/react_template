a1a5c6500aa12c4261cc54752ab751c9cf336833
ricardozarroca <ricardozarroca@gmail.com>
2021-10-30 19:46:48 +0100

    Structure, libraries and first configuration done

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    index: './src/index.js' // dont needed really
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-react',
+              {
+                runtime: 'automatic' // default: 'classic' , react_jsx_runtime mejor performance que react_createElement
+              }
+            ]
+          ]
+        }
+      }
+    ]
+  }
+
+}
